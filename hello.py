095be7e07506aa28ddc481359f89f2e38782b919
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-07-01 15:02:22 -0700

    Stub out template directory.

---
+++ hello.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+# Project auto-generated at: ...
+# Copyright (C) 2012 Mustachioed Maven.
+
+import sys
+
+from hello.main import run
+
+def main():
+    run(sys.argv)
+
+if __name__ == "__main__":
+    main()
